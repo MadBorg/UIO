aec7066f9df3efedd78939305d7a881c6c164409
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2018-11-06 21:10:53 +0100

    OPPDATE

--- MAT-INF1100/Oblig2/oppg2.py
+++ MAT-INF1100/Oblig2/oppg2.py
@@ -3,23 +3,22 @@ import matplotlib.pyplot as plt
 
 def lin_pendel_euler(v0, theta0, g, L, N, h):
     v, theta = [0]*(N+1), [0]*(N+1)
-    v[0], theta[0] = [v0], [theta0]
+    v[0], theta[0] = v0, theta0
 
     for k in range(N):
         v[k+1] = v[k] - g*h*theta[k]
-        theta[k+1] = theta[k] + h*v[k]/L
-        print(v[k+1], theta[k+1])
+        theta[k+1] = theta[k] + h*(v[k]/L)
     return v, theta
 
 
-g, L, v0, theta0, T, N = 9.81, 1, 0, np.pi/2, 4, 2**5
+g, L, v0, theta0, T, N = 9.81, 1, 0, np.pi/2, 4, 2**20
 h = T/N
 
 v, theta = lin_pendel_euler(v0, theta0, g, L, N, h)
 
-
+'''
 for i, j in zip(v, theta):
     print(i,j)
-
-#plt.plot(v, theta)
-#plt.show()
+'''
+plt.plot(theta, v)
+plt.show()
