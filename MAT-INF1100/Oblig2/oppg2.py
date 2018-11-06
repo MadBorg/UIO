2d581145c5475fcd44d7fb0b146d5db39785dba6
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2018-11-06 20:48:00 +0100

    startet på oppgave2 c og d. la også til kompendiet.

---
+++ MAT-INF1100/Oblig2/oppg2.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def lin_pendel_euler(v0, theta0, g, L, N, h):
+    v, theta = [0]*(N+1), [0]*(N+1)
+    v[0], theta[0] = [v0], [theta0]
+
+    for k in range(N):
+        v[k+1] = v[k] - g*h*theta[k]
+        theta[k+1] = theta[k] + h*v[k]/L
+        print(v[k+1], theta[k+1])
+    return v, theta
+
+
+g, L, v0, theta0, T, N = 9.81, 1, 0, np.pi/2, 4, 2**5
+h = T/N
+
+v, theta = lin_pendel_euler(v0, theta0, g, L, N, h)
+
+
+for i, j in zip(v, theta):
+    print(i,j)
+
+#plt.plot(v, theta)
+#plt.show()
--- MAT-INF1100/Oblig2/tmp.py
+++ MAT-INF1100/Oblig2/tmp.py
@@ -1,9 +1,4 @@
-def integration(x,y):
-    Y = [0]
-    for i in range(1,len(y)):
-        dx = x[i]-x[i-1] 
-        Y.append(y[i]*dx + Y[-1])
-    return Y
+
 
 
 
