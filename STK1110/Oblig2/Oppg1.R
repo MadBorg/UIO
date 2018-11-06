a8890fc5bc8599906f673f6bf6b04a93698a806c
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2018-11-06 16:36:06 +0100

    oppg1 c stk1110

--- STK1110/Oblig2/Oppg1.R
+++ STK1110/Oblig2/Oppg1.R
@@ -23,39 +23,55 @@ qqline(kvinner)
 
 '
 #c) 
-Z <- function(sm, sd, n){
-  z <- (sm[1] - sm[2])/sqrt(sd[1]**2/n[1] + sd[2]**2/n[2])
-  return(z)
-}
-
 T <- function(sm, S, N){
   m = N[1]
   n = N[2]
   X = sm[1]
   Y = sm[2]
   sp_square <- (m-1)/(m+n-2) *S[1]**2 + (n-1)/(m+n-2) * S[2]**2
-  t <- (X-Y)/sqrt(sp_square*(1/m + 1/n))
+  
   return(t)
 }
+alfa = 0.05
 
-Nmenn = length(menn)
-x = SMmenn = mean(menn)
-Smenn = sd(menn)
+n = Nmenn = length(menn)
+Y = SMmenn = mean(menn)
+Sy = Smenn = sd(menn)
 
-y = SMkvinner = mean(kvinner)
-Skvinner = sd(kvinner)
-Nkvinner = length(kvinner)
+m = Nkvinner = length(kvinner)
+X = SMkvinner = mean(kvinner)
+Sx = Skvinner = sd(kvinner)
+
 
 sm <- c(SMkvinner, SMmenn)
 S <- c(Skvinner, Smenn)
-n <- c(Nkvinner, Nmenn)
+N <- c(Nkvinner, Nmenn)
+
+sp_square <- (m-1)/(m+n-2) *Sx**2 + (n-1)/(m+n-2) * Sy**2#interpolerte variansen
+t_obs <- (X-Y)/sqrt(sp_square*(1/m + 1/n))
+
+Se1 <- Sx/sqrt(m)
+Se2 <- Sy/sqrt(n)
+v <- m+n-2 #degrees of freedom
+
+t_grense = qt(1-alfa/2,v)
+
+keep_H0 = t_grense > t_obs
+
+ci <- X - Y + c(1,-1)*qt(alfa/2, v)*sqrt(sp_square*(1/m + 1/n))
+#ser at t_obs er got utenfor Cien
+
+
+
+
+
 
 #t.test(menn, kvinner)
 #grense = 2.1 #hentet fra A.7 v = 18, alfa = 0.025. probably remove
 
 z <- Z(sm, S, n); t <- Z(sm, S, n)
 v1 = sum(n)-2
-P = 2*0.011 #hentet fra A.7, v = 18, 2.5 siden det er største tall i tabellen under 2.59
+P = 2*0.011 #hentet fra A.7, v = 18, 2.5 siden det er største tall i(kolonne/rad) tabellen under 2.59
 
 varT <- (m-1)/(m+n-2) *S[1]**2 + (n-1)/(m+n-2) * S[2]**2
 
@@ -66,7 +82,7 @@ CIT <- function(sm, n, S, alfa, v){
   X = sm[1]
   Y = sm[2]
   varT <- (m-1)/(m+n-2) *S[1]**2 + (n-1)/(m+n-2) * S[2]**2
-  ci <- X - Y + c(1,-1)*qt(alfa/2, v)*sqrt(varT*(1/m + 1/n))
+  
   return(sort(ci))
 }
 
@@ -79,3 +95,9 @@ CIT2sd <- function(sm, N, S, alfa,v){
   ci = X - Y + c(-1,1)*qt(alfa/2, v)*sqrt(sp_square*(1/m + 1/n))
   return(sort(ci))
 }
+
+#e) Utled og gjennomfør en F-test for ˚a sjekke om det er noen grunn til ˚a p˚ast˚a at
+#variansene er forskjellige. Sjekk mot var.test() i R.
+
+f = Sx**2/Sy**2
+CIf = qf(c(alfa/2, 1-alfa/2),m-1,n-1)
