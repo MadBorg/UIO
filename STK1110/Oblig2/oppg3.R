0fcf8697e26acff18ebb60f5e99d5ff4f3ad9688
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2018-11-07 11:56:03 +0100

    Stk oppg3 plots a

--- STK1110/Oblig2/oppg3.R
+++ STK1110/Oblig2/oppg3.R
@@ -1,7 +1,18 @@
 path="https://www.uio.no/studier/emner/matnat/math/STK1110/data/plastic.txt"
 plast=read.table(path,header=T)
 
-Strength = plast[1]
-Temperature = plast[2]
-Pressure = plast[3]
-qqnorm(Strength)
+
+y =Strength = plast[, 'Strength']
+x1 = Temp = plast[, 'Temperature']
+x2 = Pressure = plast[, 'Pressure']
+
+par(mfrow=c(2,2))
+qqnorm(Strength, ylab='Strength')
+qqline(Strength)
+
+boxplot(Strength, name='Strength')
+
+qqplot(x1,y, xlab='Temp')
+#qqpline(y)
+qqplot(x2,y, xlab='Pressure')
+
