c07633f079af6dd4e410a684000d33c1eba9b635
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2018-11-07 12:36:49 +0100

    stk1110 oblig2 oppg3b

--- STK1110/Oblig2/oppg3.R
+++ STK1110/Oblig2/oppg3.R
@@ -9,10 +9,25 @@ x2 = Pressure = plast[, 'Pressure']
 par(mfrow=c(2,2))
 qqnorm(Strength, ylab='Strength')
 qqline(Strength)
+#Plottet ser tilnermet normalfordelt ut.
+
 
 boxplot(Strength, name='Strength')
+#Boxplotet viser en poseti sjevhet
+
 
 qqplot(x1,y, xlab='Temp')
 #qqpline(y)
 qqplot(x2,y, xlab='Pressure')
+
+#Det ser ut som y er mer gjevnt avengig av Temp, mens det er en stor endring når Pressure er 12 som kan vise til at Styrken til plasten øker betydlig når trykket er 12.
+#Dette kan ytteliger vises med en korrelasjonskoefisient.
+
+#b)Regresjon, y(x1)
+
+x_bar = c(mean(x1), mean(x2))
+y_bar = mean(y)
+
+b1 = sum((x1-x_bar[1])*(y-mean(y)))
+b2 = sum((x2-x_bar[2])*(y-mean(y)))
 
