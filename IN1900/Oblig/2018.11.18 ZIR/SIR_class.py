4683023910ccb428b46f580c68afef6ec26b120e
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2018-11-12 23:05:13 +0100

    IN1900 2018.11.18 ZIR SIR_class.py Traceback (most recent call last):   File "D:\OneDrive - Universitetet i Oslo\UIO\IN1900\Oblig\2018.11.18 ZIR\SIR_class.py", line 83, in <module>     print(solverb1.solve())   File "D:\OneDrive - Universitetet i Oslo\UIO\IN1900\Oblig\2018.11.18 ZIR\SIR_class.py", line 57, in solve     u, self.t = self.solver.solve(t)   File "D:\OneDrive - Universitetet i Oslo\UIO\IN1900\Oblig\2018.11.18 ZIR\ODESolver.py", line 64, in solve     if self.neq == 1:  # scalar ODEs AttributeError: 'RungeKutta4' object has no attribute 'neq'

---
+++ IN1900/Oblig/2018.11.18 ZIR/SIR_class.py
@@ -0,0 +1,85 @@
+
+import ODESolver
+import numpy as np
+import matplotlib.pyplot as plt
+
+class ProblemSIR(object):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import ODESolver
+
+    def __init__(self, beta, nu, S0, I0, R0, T):
+        '''
+        nu, beta: parameters in the ODE system
+        S0, I0, R0: initial values
+        T: simulation for t in [0,T]
+        '''
+        self.U0 = [S0, I0, R0]
+        self.T = T
+
+        if isinstance(nu, (float, int)): #number?
+            self.nu =lambda t: nu #wrap as function
+        elif callable(nu):
+            self.nu = nu
+        #else:
+        #    print('Error in init: self.nu')
+
+        if isinstance(beta, (float, int)): #number?
+            self.beta =lambda t: beta #wrap as function
+        elif callable(beta):
+            self.beta = beta
+        #else:
+        #    print('Error in init: self.beta')
+
+
+
+    def __call__(self, u, t):
+        beta, nu, U0 = self.beta, self.nu, self.U0
+        S, I, R = u
+        return [-beta *S*I, beta*S*I - nu*I, nu*I]
+
+    def terminate(self, u, t, step_no, tol=1e-10):
+        return abs(sum(u[step_no])-sum(self.U0)) > tol
+        #return False #True
+
+class SolverSIR(object):
+    import ODESolver
+    import numpy as np
+    import matplotlib.pyplot as plt
+    def __init__(self, problem, dt):
+        self.problem, self.dt = problem, dt
+
+    def solve(self, method=ODESolver.RungeKutta4):
+        self.solver = method(self.problem)
+        ic = self.problem.U0
+        n = int(round(self.problem.T/float(self.dt)))
+        t = np.linspace(0, self.problem.T, n+1)
+        u, self.t = self.solver.solve(t)
+        self.S, self.I, self.R = u[:,0], u[:,1], u[:,2]
+
+    def plot(self, title=None, xlabel=None, yabel=None):
+        S,I,R = self.u
+        t = self.t
+        plt.gca().set_title(title)
+        plt.plot(t, S, 'k-', t, I, 'b-', t, R, 'r-')
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.legend(['S', 'I', 'R'], loc = 'lower right')
+        #plt.show()
+
+def beta(t):
+    if 0 <= t <= 12:
+        return 0.0005
+    if t > 12:
+        return 0.0001
+beta2 = 0.0005
+
+dt = 0.1
+nu = 0.1
+S0, I0, R0 = 1500, 1, 0
+T = 60 #days
+problemb1 = ProblemSIR(beta, nu, S0, I0, R0, T)
+solverb1 = SolverSIR(problemb1,dt)
+print(solverb1.solve())
+#problemb2 = ProblemSIR(beta2, nu, S0, I0, R0, T)
+#solverb2 = SolverSIR(problemb2,dt)
